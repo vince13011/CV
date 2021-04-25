f8061f2f5290f8d3b3cb3a150c39709a00ee3eb3
vincent giglio <vincent.giglio@hotmail.fr>
2021-04-25 11:40:21 +0200

    responsive menu is ready

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,18 +13,18 @@ const Menu = ({ categories }) => {
 
   return (
     <>
-      <li key='Accueil' className="header__navlink">
+      <li key='Accueil' className="header__navlink" >
         <NavLink
-          
+          activeClassName="active"
           to={`/`}
           exact
         >
           Accueil
           </NavLink>
       </li>
-      <li key='Expériences' className="header__navlink">
+      <li key='Expériences' className="header__navlink" >
         <NavLink
-          
+          activeClassName="active"
           to={`/categories/experiences`}
           exact
         >
@@ -33,7 +33,7 @@ const Menu = ({ categories }) => {
       </li>
       <li key='Diplomes' className="header__navlink">
         <NavLink
-          
+          activeClassName="active"
           to={`/categories/diplomes`}
           exact
         >
@@ -44,7 +44,7 @@ const Menu = ({ categories }) => {
       {categories.map(({ name, id }) => (
         <li key={id} className="header__navlink">
           <NavLink
-            
+            activeClassName="active"
             to={`/categories/${name}`}
             exact
           >

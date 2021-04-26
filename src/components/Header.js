b0a6c094d4e68ca22cd8bdce23669f4fc765ad70
vincent giglio <vincent.giglio@hotmail.fr>
2021-04-26 17:59:40 +0200

    netlify errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = ({ categories }) => {
     <header className="header">
       <div className="header__presentation">
         <div className="header__presentation__portrait">
-          <img className="header__presentation__portrait__image" src={CVPicture} alt="photo CV" />
+          <img className="header__presentation__portrait__image" src={CVPicture} alt="CV" />
           <h2 className="header__presentation__portrait__name">Vincent Giglio <br/>26 ans</h2>
           <ExtLinks className="extlinks"/>
          </div>
